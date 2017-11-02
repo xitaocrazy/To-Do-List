1f172058e6b362557d21fba8fa9a768e1ecf256b
Daniel "Xitão" Martins <xitaocrazy@gmail.com>
2017-11-02 20:40:56 -0200

    Working with localStorage
    Working with localStorage to store the to-do and done lists.

--- ts/built/src/indexViewModel.js
+++ ts/built/src/indexViewModel.js
@@ -8,6 +8,14 @@ var main;
                 this.markAsDone = function (item) {
                     _this.toDoList.remove(item);
                     _this.doneList.push(item);
+                    if (typeof (Storage) !== "undefined") {
+                        localStorage.removeItem(item.title());
+                        item.title(item.title() + '_Done');
+                        localStorage.setItem(item.title(), item.description());
+                    }
+                    else {
+                        item.title(item.title() + '_Done');
+                    }
                 };
                 this.title = ko.observable('');
                 this.description = ko.observable('');
@@ -26,10 +34,23 @@ var main;
             };
             ;
             IndexViewModel.prototype.getTodoList = function () {
-                var a = { title: ko.observable('Teste1'), description: ko.observable('Teste1') };
-                var b = { title: ko.observable('Teste1'), description: ko.observable('Teste1') };
-                this.toDoList.push(a);
-                this.toDoList.push(b);
+                if (typeof (Storage) !== "undefined") {
+                    for (var i = 0; i < localStorage.length; i++) {
+                        var key = localStorage.key(i);
+                        var value = localStorage.getItem(key);
+                        var item = {
+                            title: ko.observable(key),
+                            description: ko.observable(value)
+                        };
+                        var isDone = /_Done$/i.test(key);
+                        if (isDone) {
+                            this.doneList.push(item);
+                        }
+                        else {
+                            this.toDoList.push(item);
+                        }
+                    }
+                }
             };
             ;
             IndexViewModel.prototype.validateData = function () {
@@ -42,6 +63,9 @@ var main;
                     title: ko.observable(this.title()),
                     description: ko.observable(this.description())
                 };
+                if (typeof (Storage) !== "undefined") {
+                    localStorage.setItem(item.title(), item.description());
+                }
                 this.toDoList.push(item);
                 this.title('');
                 this.description('');
--- ts/src/indexViewModel.ts
+++ ts/src/indexViewModel.ts
@@ -33,13 +33,34 @@ module main.modules {
         public markAsDone = (item: ITodoItem) =>{
             this.toDoList.remove(item);
             this.doneList.push(item);
+            if (typeof(Storage) !== "undefined") {
+                localStorage.removeItem(item.title());
+                item.title(item.title() + '_Done');
+                localStorage.setItem(item.title(), item.description());
+            }  else{
+                item.title(item.title() + '_Done');
+            }                                             
         };
 
-        private getTodoList(){
-            var a = {title:  ko.observable('Teste1'), description:  ko.observable('Teste1') };
-            var b = {title:  ko.observable('Teste1'), description:  ko.observable('Teste1') };
-            this.toDoList.push(a);
-            this.toDoList.push(b);
+        private getTodoList(){ 
+            if (typeof(Storage) !== "undefined") {
+                for (var i = 0; i < localStorage.length; i++) {
+                    var key = localStorage.key(i);
+                    var value = localStorage.getItem(key);
+                    var item = {
+                        title:  ko.observable(key), 
+                        description:  ko.observable(value) 
+                    };
+                    
+                    var isDone = /_Done$/i.test(key);
+    
+                    if (isDone){
+                        this.doneList.push(item);
+                    }else{
+                        this.toDoList.push(item);
+                    }
+                }
+            }                    
         };
 
         private validateData(){
@@ -53,6 +74,9 @@ module main.modules {
                 title:  ko.observable(this.title()), 
                 description:  ko.observable(this.description()) 
             };
+            if (typeof(Storage) !== "undefined") {
+                localStorage.setItem(item.title(), item.description());
+            }             
             this.toDoList.push(item);
             this.title('');
             this.description('');
